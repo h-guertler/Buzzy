ff65805120fe67d7dea3cf3defeaa3a60c9c31f3
h-guertler <guertler99@gmail.com>
2024-02-26 14:43:31 -0500

    updated seeder data

--- app/seeds/event_images.py
+++ app/seeds/event_images.py
@@ -4,15 +4,15 @@ from sqlalchemy.sql import text
 
 def seed_event_images():
     image_one = Event_Image(
-        user_id=1, event_id=2, url='imageone.fake.com')
+        user_id=1, event_id=1, url='https://upload.wikimedia.org/wikipedia/en/f/ff/Morning_Glory_Flower_square.jpg')
     image_two = Event_Image(
-        user_id=2, event_id=1, url='imagetwo.fake.com')
+        user_id=2, event_id=1, url='https://mypoppet.com.au/makes/mp-content/uploads/2020/02/magic-yarn-ball_2.jpg')
     image_three = Event_Image(
-        user_id=1, event_id=2, url='imagethree.fake.com')
+        user_id=1, event_id=2, url='https://assets.simpleviewinc.com/simpleview/image/upload/c_limit,h_1200,q_75,w_1200/v1/clients/asheville/Sierra_Nevada_Resize_FB_06a29a19-7eaa-4cd1-85a6-c85afa233342.jpg')
     image_four = Event_Image(
-        user_id=1, event_id=3, url='https://images.fineartamerica.com/images-medium-large-5/rachel-dahlia-flower-square-barbara-griffin.jpg')
+        user_id=2, event_id=3, url='https://cdn.britannica.com/82/203482-050-E2ABDA79/People-fireworks.jpg')
     image_five = Event_Image(
-        user_id=1, event_id=3, url='https://upload.wikimedia.org/wikipedia/en/f/ff/Morning_Glory_Flower_square.jpg')
+        user_id=1, event_id=3, url='https://cdn.pixabay.com/photo/2017/07/21/23/57/concert-2527495_1280.jpg')
 
     db.session.add(image_one)
     db.session.add(image_two)
